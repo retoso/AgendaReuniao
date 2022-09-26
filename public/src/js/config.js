2aaf913b6350abd835baa599378dfa1699fd77fe
Renato Tonelli de Souza <renato.souza1969@yahoo.com.br>
2022-09-26 19:44:02 -0300

    Formulario vinculado com firebase host

---
+++ public/src/js/config.js
@@ -0,0 +1,11 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyBA5Ags5_bGq94FdaLYfHU2RlhRaX_QrjE",
+    authDomain: "agendatest-32fc0.firebaseapp.com",
+    projectId: "agendatest-32fc0",
+    storageBucket: "agendatest-32fc0.appspot.com",
+    messagingSenderId: "847568347797",
+    appId: "1:847568347797:web:948f15d174d9cf3b85b19c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
